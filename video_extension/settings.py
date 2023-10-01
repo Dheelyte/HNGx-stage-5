e4219f1cafae7a5d418b50a87555ecc33b0884f9
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-10-01 22:12:07 +0100

    Fix static files storage

--- video_extension/settings.py
+++ video_extension/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a#w=shzrfj&#%rhvnz7kq(9dk05@-8nj$v*d8d+azwqet(0^-)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'video-transcribe-ai.onrender.com']
 
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # core/settings.py
 
@@ -132,6 +134,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# SWAGGER_SETTINGS = {
-#    'USE_SESSION_AUTH': False
-# }
+SWAGGER_SETTINGS = {
+   'USE_SESSION_AUTH': False
+}
