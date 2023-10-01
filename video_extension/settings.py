f9d99a255b455a2c84e9f4ca79e5d7264f4fc012
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-10-01 22:05:17 +0100

    Add STATIC_ROOT

--- video_extension/settings.py
+++ video_extension/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # core/settings.py
 
@@ -131,6 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SWAGGER_SETTINGS = {
-   'USE_SESSION_AUTH': False
-}
+# SWAGGER_SETTINGS = {
+#    'USE_SESSION_AUTH': False
+# }
